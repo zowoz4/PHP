e8a32b7720b2ed32f85ec1a350abf8563fd9f0ee
zowoz4 <46922103+zowoz4@users.noreply.github.com>
2019-02-17 17:59:34 +0900

    first version

---
+++ conditional.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>Conditional</h1>
+    <h2>if</h2>
+    <?php
+      echo '1<br>';
+      if(false) {
+        echo '2-1<br>';
+      } else {
+        echo '2-2<br>';
+      }
+      echo '3<br>';
+     ?>
+  </body>
+</html>
